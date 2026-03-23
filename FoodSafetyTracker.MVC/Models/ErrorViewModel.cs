9341f785fd6a438cefe1c6b97b60bcedf158c859
Manar <esdibmanar@gmail.com>
2026-03-23 17:03:25 +0000

    Agregar archivos de proyecto.

---
+++ FoodSafetyTracker.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodSafetyTracker.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
